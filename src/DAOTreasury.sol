1362cd74b7a466f8c3a404c6e7724f4e16278b92
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2026-08-24 10:11:31 -0500

    feat: implement DAOTreasury contract for managing ETH and ERC20 funds with emergency withdrawal capabilities

--- src/DAOTreasury.sol
+++ src/DAOTreasury.sol
@@ -39,8 +39,57 @@ contract DAOTreasury is Ownable {
 	}
 
 	function setDAO(address _dao) external onlyOwner {
-
+		require(_dao != address(0), "DAO address cannot be zero");
+		dao = DAO(_dao);
+		emit DAOSet(_dao);
 	}
 
+
+	/**
+	 * @dev Fund the Trasuery with ETH 
+	 */
+	function fundTreasury() external payable {
+		require(msg.value > 0, "Must send ETH");
+		emit TreasuryFunded(msg.sender, msg.value);
+	}
+
+	/**
+	 * @dev Fund the treasury with ERC20 tokens
+	 * @param token Token address
+	 * @param amount Amount to fund 
+	 */
+
+	function fundTreasuryWithToken(address token, uint256 amount) external {
+		require(token != address(0), "Invalid token address");
+		require(amount > 0, "Amount must be greater than 0");
+
+		IERC20 tokenContract = IERC20(token);
+		require(tokenContract.transferFrom(msg.sender, address(this), amount), "Failed to transfer tokens");
+		
+		emit TreasuryFunded(msg.sender, amount);
+	}
+
+	//Allow contract to receive ETH 
+	receive() external payable {
+		emit TreasuryFunded(msg.sender, msg.value);
+	}
+
+	function emergencyWithdraw(address token, uint256 amount, address receipient) external onlyOwner() {
+		require(receipient != address(0), "Invalid recipient address");
+		require(amount > 0, "Amount must be greater than 0");
+
+		if(token == address(0)) {
+			require(address(this).balance >= amount, "Insufficient ETH balance");
+			(bool success,) = receipient.call{value: amount}("");
+			require(success, "ETH transfer failed");
+		} else {
+			IERC20 tokenContract = IERC20(token);
+			require(tokenContract.balanceOf(address(this)) >= amount, "Insufficient token balance");
+			require(tokenContract.transfer(receipient, amount), "Token transfer failed");
+		}
+		
+	}
+
+	
     
 }
